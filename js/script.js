472191d679d74b69a3191586120f3034cb1fac67
Adam Lynch <contact@adamlynch.ie>
2013-05-14 23:22:14 +0200

    Here comes the serif
    Added serifs plus little JS fixes

--- js/script.js
+++ js/script.js
@@ -4,8 +4,11 @@ $( 'body' ).on('click', '.sidebar', function(){
 	.on('click', '.post', function(){
 		$('.sidebar').removeClass('active');
 	})
-	.find('.logo a' ).on('click', function(e){
+	.find('.sidebar a').on('click', function(e){
 		e.stopPropagation();
+		if( '#' === this.href || '#' === this.href.substr(-1) && -1 < this.href.indexOf(window.location.href)){
+			e.preventDefault();
+		}
 	})
 	.end()
 	.find('.post').mousewheel( function( e, delta ){
